f8fc84c583ff7f2533d19fdaf0700db0cc823b37
VJmr <jaganvudumula@gmail.com>
2018-06-08 18:12:44 +0530

    Added states and parties constant for further data manipulation

---
+++ src/app/shared/models/parties.ts
@@ -0,0 +1,143 @@
+export const Parties=[
+	{
+		"partyName": "Bahujan Samaj Party"
+	},
+	{
+		"partyName": "Bharatiya Janata Party"
+	},
+	{
+		"partyName": "Communist Party of India"
+	},
+	{
+		"partyName": "Communist Party of India (Marxist)"
+	},
+	{
+		"partyName": "Indian National Congress"
+	},
+	{
+		"partyName": "Nationalist Congress Party"
+	},
+	{
+		"partyName": "Aam Aadmi Party"
+	},
+	{
+		"partyName": "AJSU Party"
+	},
+	{
+		"partyName": "All India Anna Dravida Munnetra Kazhagam"
+	},
+	{
+		"partyName": "All India Forward Bloc"
+	},
+	{
+		"partyName": "All India N.R. Congress"
+	},
+	{
+		"partyName": "All India Trinamool Congress"
+	},
+	{
+		"partyName": "All India United Democratic Front"
+	},
+	{
+		"partyName": "Asom Gana Parishad"
+	},
+	{
+		"partyName": "Biju Janata Dal"
+	},
+	{
+		"partyName": "Bodoland Peoples Front"
+	},
+	{
+		"partyName": "Desiya Murpokku Dravida Kazhagam"
+	},
+	{
+		"partyName": "Dravida Munnetra Kazhagam"
+	},
+	{
+		"partyName": "Haryana Janhit Congress (BL)"
+	},
+	{
+		"partyName": "Indian National Lok Dal"
+	},
+	{
+		"partyName": "Indian Union Muslim League"
+	},
+	{
+		"partyName": "Jammu & Kashmir National Conference"
+	},
+	{
+		"partyName": "Jammu & Kashmir National Panthers Party"
+	},
+	{
+		"partyName": "Jammu & Kashmir Peoples Democratic Party"
+	},
+	{
+		"partyName": "Janata Dal (Secular)"
+	},
+	{
+		"partyName": "Janata Dal (United)"
+	},
+	{
+		"partyName": "Jharkhand Mukti Morcha"
+	},
+	{
+		"partyName": "Jharkhand Vikas Morcha (Prajatantrik)"
+	},
+	{
+		"partyName": "Kerala Congress (M)"
+	},
+	{
+		"partyName": "Lok Jan Shakti Party"
+	},
+	{
+		"partyName": "Maharashtra Navnirman sena"
+	},
+	{
+		"partyName": "Naga Peoples Front"
+	},
+	{
+		"partyName": "National Peoples Party"
+	},
+	{
+		"partyName": "Pattali Makkal Katchi"
+	},
+	{
+		"partyName": "People's Party of Arunachal"
+	},
+	{
+		"partyName": "Rashtriya Janata Dal"
+	},
+	{
+		"partyName": "Rashtriya Lok Dal"
+	},
+	{
+		"partyName": "Revolutionary Socialist Party"
+	},
+	{
+		"partyName": "Samajwadi Party"
+	},
+	{
+		"partyName": "Shiromani Akali Dal"
+	},
+	{
+		"partyName": "Shivsena"
+	},
+	{
+		"partyName": "Sikkim Democratic Front"
+	},
+	{
+		"partyName": "Telangana Rashtra Samithi"
+	},
+	{
+		"partyName": "Telugu Desam"
+	},
+	{
+		"partyName": "United Democratic Party"
+	},
+	{
+		"partyName": "Unrecognised"
+	},
+	{
+		"partyName": "Independent"
+	}
+]
---
+++ src/app/shared/models/states.ts
@@ -0,0 +1,107 @@
+export const States = [
+	{
+		"stateName": "Andaman & Nicobar Islands"
+	},
+	{
+		"stateName": "Andhra Pradesh"
+	},
+	{
+		"stateName": "Arunachal Pradesh"
+	},
+	{
+		"stateName": "Assam"
+	},
+	{
+		"stateName": "Bihar"
+	},
+	{
+		"stateName": "Chandigarh"
+	},
+	{
+		"stateName": "Chhattisgarh"
+	},
+	{
+		"stateName": "Dadra & Nagar Haveli"
+	},
+	{
+		"stateName": "Daman & Diu"
+	},
+	{
+		"stateName": "Goa"
+	},
+	{
+		"stateName": "Gujarat"
+	},
+	{
+		"stateName": "Haryana"
+	},
+	{
+		"stateName": "Himachal Pradesh"
+	},
+	{
+		"stateName": "Jammu & Kashmir"
+	},
+	{
+		"stateName": "Jharkhand"
+	},
+	{
+		"stateName": "Karnataka"
+	},
+	{
+		"stateName": "Kerala"
+	},
+	{
+		"stateName": "Lakshadweep"
+	},
+	{
+		"stateName": "Madhya Pradesh"
+	},
+	{
+		"stateName": "Maharashtra"
+	},
+	{
+		"stateName": "Manipur"
+	},
+	{
+		"stateName": "Meghalaya"
+	},
+	{
+		"stateName": "Mizoram"
+	},
+	{
+		"stateName": "NCT OF Delhi"
+	},
+	{
+		"stateName": "Nagaland"
+	},
+	{
+		"stateName": "Odisha"
+	},
+	{
+		"stateName": "Puducherry"
+	},
+	{
+		"stateName": "Punjab"
+	},
+	{
+		"stateName": "Rajasthan"
+	},
+	{
+		"stateName": "Sikkim"
+	},
+	{
+		"stateName": "Tamil Nadu"
+	},
+	{
+		"stateName": "Tripura"
+	},
+	{
+		"stateName": "Uttar Pradesh"
+	},
+	{
+		"stateName": "Uttarakhand"
+	},
+	{
+		"stateName": "West Bengal"
+	}
+]
