dbf6f328c0d080b28af2b1b5d9d2a6f2f2bbead0
VJmr <jaganvudumula@gmail.com>
2018-06-08 20:06:36 +0530

    colors to parties

--- src/app/shared/models/parties.ts
+++ src/app/shared/models/parties.ts
@@ -1,143 +1,190 @@
 export const Parties=[
 	{
-		"partyName": "Bahujan Samaj Party"
+    "partyName": "Bahujan Samaj Party",
+    "color":"#0000FF"
 	},
 	{
-		"partyName": "Bharatiya Janata Party"
+    "partyName": "Bharatiya Janata Party",
+    "color":"#FF9933"
 	},
 	{
-		"partyName": "Communist Party of India"
+    "partyName": "Communist Party of India",
+    "color":"#FF000"
 	},
 	{
-		"partyName": "Communist Party of India (Marxist)"
+    "partyName": "Communist Party of India (Marxist)",
+    "color":"#FF000"
 	},
 	{
-		"partyName": "Indian National Congress"
+    "partyName": "Indian National Congress",
+    "color": "#80DAEB"
 	},
 	{
-		"partyName": "Nationalist Congress Party"
+    "partyName": "Nationalist Congress Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Aam Aadmi Party"
+    "partyName": "Aam Aadmi Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "AJSU Party"
+    "partyName": "AJSU Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "All India Anna Dravida Munnetra Kazhagam"
+    "partyName": "All India Anna Dravida Munnetra Kazhagam",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "All India Forward Bloc"
+    "partyName": "All India Forward Bloc",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "All India N.R. Congress"
+    "partyName": "All India N.R. Congress",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "All India Trinamool Congress"
+    "partyName": "All India Trinamool Congress",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "All India United Democratic Front"
+    "partyName": "All India United Democratic Front",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Asom Gana Parishad"
+    "partyName": "Asom Gana Parishad",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Biju Janata Dal"
+    "partyName": "Biju Janata Dal",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Bodoland Peoples Front"
+    "partyName": "Bodoland Peoples Front",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Desiya Murpokku Dravida Kazhagam"
+    "partyName": "Desiya Murpokku Dravida Kazhagam",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Dravida Munnetra Kazhagam"
+    "partyName": "Dravida Munnetra Kazhagam",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Haryana Janhit Congress (BL)"
+    "partyName": "Haryana Janhit Congress (BL)",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Indian National Lok Dal"
+    "partyName": "Indian National Lok Dal",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Indian Union Muslim League"
+    "partyName": "Indian Union Muslim League",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Jammu & Kashmir National Conference"
+    "partyName": "Jammu & Kashmir National Conference",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Jammu & Kashmir National Panthers Party"
+    "partyName": "Jammu & Kashmir National Panthers Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Jammu & Kashmir Peoples Democratic Party"
+    "partyName": "Jammu & Kashmir Peoples Democratic Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Janata Dal (Secular)"
+    "partyName": "Janata Dal (Secular)",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Janata Dal (United)"
+    "partyName": "Janata Dal (United)",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Jharkhand Mukti Morcha"
+    "partyName": "Jharkhand Mukti Morcha",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Jharkhand Vikas Morcha (Prajatantrik)"
+    "partyName": "Jharkhand Vikas Morcha (Prajatantrik)",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Kerala Congress (M)"
+    "partyName": "Kerala Congress (M)",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Lok Jan Shakti Party"
+    "partyName": "Lok Jan Shakti Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Maharashtra Navnirman sena"
+    "partyName": "Maharashtra Navnirman sena",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Naga Peoples Front"
+    "partyName": "Naga Peoples Front",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "National Peoples Party"
+    "partyName": "National Peoples Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Pattali Makkal Katchi"
+    "partyName": "Pattali Makkal Katchi",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "People's Party of Arunachal"
+    "partyName": "People's Party of Arunachal",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Rashtriya Janata Dal"
+    "partyName": "Rashtriya Janata Dal",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Rashtriya Lok Dal"
+    "partyName": "Rashtriya Lok Dal",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Revolutionary Socialist Party"
+    "partyName": "Revolutionary Socialist Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Samajwadi Party"
+    "partyName": "Samajwadi Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Shiromani Akali Dal"
+    "partyName": "Shiromani Akali Dal",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Shivsena"
+    "partyName": "Shivsena",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Sikkim Democratic Front"
+    "partyName": "Sikkim Democratic Front",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Telangana Rashtra Samithi"
+    "partyName": "Telangana Rashtra Samithi",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Telugu Desam"
+    "partyName": "Telugu Desam",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "United Democratic Party"
+    "partyName": "United Democratic Party",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Unrecognised"
+    "partyName": "Unrecognised",
+    "color": "#007FFF"
 	},
 	{
-		"partyName": "Independent"
+    "partyName": "Independent",
+    "color": "#007FFF"
 	}
 ]
