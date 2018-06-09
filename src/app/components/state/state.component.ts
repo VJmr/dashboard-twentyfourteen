f848e5bf44b22b7cf3ccadd4ec02e541580d0c73
VJmr <jaganvudumula@gmail.com>
2018-06-10 00:05:16 +0530

    Added ddl and icon

--- src/app/components/state/state.component.ts
+++ src/app/components/state/state.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ResultsService } from '../../shared/services/results.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import { Results, States, Parties } from '../../shared/models';
 
 @Component({
   selector: 'app-state',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./state.component.css']
 })
 export class StateComponent implements OnInit {
-
-  constructor() { }
+  results: Observable<Results>;
+  states = States;
+  parties = Parties;
+  constructor(private resultsService: ResultsService) { }
 
   ngOnInit() {
+    this.results= this.resultsService.getResults().map(res=>{
+      return res.map(item=>{
+        return new Results(
+          item.partyName,
+          item.partyType,
+          item.stateName,
+          +item.totalVotesPolledInState,
+          +item.totalVotesInState,
+          +item.seatsWon,
+          +item.totalVotesPolledInStateForParty,
+          +item.totalVotesPolledInStateForPartyPercentage
+        )
+      })
+    });
   }
 
 }
